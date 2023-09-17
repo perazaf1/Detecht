7872c28f48afab6175faf0827ba8c907232a1c4d
perazaf1 <pe.razafindrakoto@gmail.com>
2023-09-17 22:07:24 +0200

    use of js library typewriter

--- src/main.js
+++ src/main.js
@@ -13,3 +13,28 @@ var loader = document.getElementById('preloader')
 window.addEventListener('load', function(){
     loader.style.display = "none"
 })
+
+const txtAnim = document.getElementById('txtAnim');
+
+new Typewriter(txtAnim, {
+    deleteSpeed: 5
+  })
+
+  .typeString("With our products,")
+  .pauseFor(200)
+  .typeString('<span style="color: #709ca7"> no more rapes </span>')
+  .pauseFor(1000)
+  .deleteChars(15)
+  .typeString('<span style="color: #709ca7"> no more violence </span>')
+  .pauseFor(1000)
+  .deleteChars(18)
+  .typeString('<span style="color: #709ca7"> no more pilferage </span>')
+  .pauseFor(1000)
+  .deleteChars(19)
+  .typeString('<span style="color: #709ca7"> we all get a better life</span>')
+  .start();
+
+  
+  
+
+
