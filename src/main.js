c9542b527fca56e95f27b75b3609baa590a78696
perazaf1 <pe.razafindrakoto@gmail.com>
2023-09-19 09:50:07 +0200

    css changes

--- src/main.js
+++ src/main.js
@@ -31,7 +31,7 @@ new Typewriter(txtAnim, {
   .typeString('<span style="color: #709ca7"> no more pilferage </span>')
   .pauseFor(1000)
   .deleteChars(19)
-  .typeString('<strong style="color: #709ca7"> we all get a better life !</strong>')
+  .typeString('<strong style="color: #709ca7"> we all get a gentle life</strong>')
   .start();
 
   
